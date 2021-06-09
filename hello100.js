cc0399dfabf56fd1a454824f042d0a2ef7411cf9
toinfinityNbeyond <the_1975@naver.com>
2021-06-09 16:42:10 +0900

    test new3

--- hello100.js
+++ hello100.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-console.log("Hdsfadsf!");
-console.log("Tasdfsdfasdf1");
-console.log("Teasdfsdfasdfdsafasfasd2");
-=======
 console.log("Hello, world!");
-console.log("test4");
-console.log("test3");
 console.log("Test1");
 console.log("Test2");
->>>>>>> f03cbc7585fb119d222e2c2786182bf7986f8753
