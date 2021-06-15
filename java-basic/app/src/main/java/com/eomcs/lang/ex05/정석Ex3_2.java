b3b14048870404ac6bd7879b7bff454b009cfaab
unknown <the_1975@naver.com>
2021-06-15 20:24:50 +0900

    0615

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex05/정석Ex3_2.java
@@ -0,0 +1,18 @@
+package com.eomcs.lang.ex05;
+
+public class Ex3_2 {
+    public static void main (String[] args) {
+    
+    int i = 5 , j= 0 ; //후위형
+
+    j = i; 
+    i++;
+    System.out.println("j=i++; 실행 후 , i=" +  i + ", j= " + j);
+
+    i = 5;  // 결과를 비교하기 위해, i와 j 의 값을 다시 5와 0으로 변경
+    j = 0;
+
+    j = ++i; // 전위형
+    System.out.println("j=++i; 실행 후, i=" + i +", j="+j);
+    }
+}
