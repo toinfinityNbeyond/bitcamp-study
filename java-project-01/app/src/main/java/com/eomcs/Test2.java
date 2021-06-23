4372256768d47ccd2c2edd5dd3715302ab26670d
unknown <the_1975@naver.com>
2021-06-23 19:18:55 +0900

    0623

--- java-project-01/app/src/main/java/com/eomcs/Test2.java
+++ java-project-01/app/src/main/java/com/eomcs/Test2.java
@@ -1,30 +1,33 @@
 package com.eomcs;
 
-class A {}
-class B {}
-class C {}
-
 public class Test2 {
+
+
+  static class A {
+    int x;
+  }
+  static class B {
+    int y;
+  }
+  static class C {
+    int z;
+  }
   public static void main (String[] args) {
+    A r1=  new A();
+    r1.x = 100;
 
-    A r1;
-    B r2;
-    C r3;
 
-    r1 = new A();
-    //     r1 = new B();
-    //     r1 = new C();
-    //     r2 = new A ();
-    r2 = new B ();
-    //     r2 = new C ();
+    Object obj = new A();
+    //obj.x = 100; // x에는 실제 A의 인스턴스 주소가 저장된 것이 맞지만,
+    // 컴파일러 입장에서는 obj 변수의 클래스를 가지고 변수를 찾기 때문에 
+    // 컴파일 오류가 발생한다.
 
-    //     r3 = newA ();
-    //     r3= new B ();
-    r3 = new C();
+    //obj에 들어 있는 인스턴스 주소를 제대로 사용하려면
+    // 원래의 타입으로 형변환 한 다음에 사용하라.
+    A r2 =(A) obj;
+    r2.x = 100; //OK!
 
-    Object obj;
-    obj = new A();
-    obj = new B();
-    obj = new C();
+
+
   }
 }
