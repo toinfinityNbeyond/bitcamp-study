d83b413ad0beb7d3f763e84e7bb43f21e45ecad0
unknown <the_1975@naver.com>
2021-06-23 17:25:45 +0900

    0623

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex00/Exam00.java
@@ -0,0 +1,13 @@
+package com.eomcs.lang.ex00;
+
+
+
+public class Exam00 {
+  static public void main (String[] args ) {
+    //  public final static void main(String[] args) {
+    //  public static void main(String[] args) {
+    String s4 = "30";
+    int i = 20;
+    System.out.println(s4 + i);
+  }
+}
--- java-project-01/app/src/main/java/com/eomcs/App.java
+++ java-project-01/app/src/main/java/com/eomcs/App.java
@@ -1,6 +1,8 @@
 package com.eomcs;
 
+import java.util.Date;
 import java.util.Scanner;
+
 
 //01. main(): 프로그램 entry point
 //02. 사용자로부터 명령어 입력 받기
@@ -18,33 +20,166 @@ import java.util.Scanner;
 //14. 게시글 삭제하기
 //15. 메서드 문법을 이용하여 기능 별로 명령어를 묶어 관리하기
 //16. 클래스를 이용하여 새 데이터타입을 정의하기
+//17. 클래스를 이용하여 메서드를 분류하기
+//18. 데이터 목록을 다루는 코드를 재사용하기 쉽게 별도의 클래스로 분리한다.
 public class App {
-  public static void main(String[] args) {
-    System.out.println("[게시판 관리]");
 
-    Scanner scanner = new Scanner(System.in); 
-    System.out.print("명령 → ");
-    String s = scanner.nextLine();
-    scanner.close();
-
-    switch (s) {
-      case "list":
-        System.out.println("[게시판 목록]");
-        break;
-      case "name":
-        System.out.println("[게시판 이름]");
-        break;
-      case "update":
-        break;
-      default :
-        System.out.println("지원하지 않는 명령입니다.");
-    }
-    System.out.println("입력을 완료했습니다.");
+  // 한 개의 게시글을 담을 복합 데이터의 변수를 설계
+  static class Board {
+    String title;
+    String content;
+    String password;
+    int viewCount;
+    Date createdDate;
   }
 
+  static Scanner keyScan = new  Scanner(System.in); 
+
+
+  public static void main(String[] args) {
+    // App 클래스에서 만든 Scanner 인스턴스를 BoardHandler 같이 쓴다.
+    BoardHandler.keyScan = keyScan; 
+
+    System.out.println("[게시판 관리]");
+
+
+    loop: while (true) {
+      System.out.print("명령> ");
+      String command = keyScan.nextLine();
+
+      switch (command) {
+        case "list": BoardHandler.list(); break;
+        case "add": BoardHandler.add(); break;
+        case "update": BoardHandler.update(); break;
+        case "delete": BoardHandler.delete(); break;
+        case "view": BoardHandler.view(); break;
+        case "quit":
+          break loop;
+        default:
+          System.out.println("지원하지 않는 명령입니다.");
+      }
+    }
+    keyScan.close();
+
+    System.out.println("안녕히 가세요!"); 
+  }
+
+  //  static void list() {
+  //    System.out.println("[게시글 목록]");
+  //
+  //    for (int i = 0; i < size; i++) {
+  //      Board board = boards[i];
+  //      System.out.println("%d, %s, %s, %d%n",
+  //          i,
+  //          board.title,
+  //          String.format("%1$tY-%1$tm-%1$td", board.createdDate),
+  //          board.viewCount);
+  //    }
+  //  }
+  //
+  //  static void add() {
+  //    System.out.println("[게시글 등록]");
+  //
+  //    if (size == BOARD_LENGTH) {
+  //      System.out.println("더이상 게시글을 추가할 수 없습니다.");
+  //      return;
+  //    }
+  //
+  //    Board board = new Board();
+  //
+  //    System.out.print("제목: ");
+  //    board.title = keyScan.nextLine();
+  //
+  //    System.out.print("내용: ");
+  //    board.content = keyScan.nextLine();
+  //
+  //    System.out.print("비밀번호: ");
+  //    board.password = keyScan.nextLine();
+  //
+  //    board.createdDate = new Date(); //  new Date도 class
+  //
+  //    boards[size++] = board;
+  //
+  //
+  //    System.out.println("게시글을 등록했습니다.");
+  //  }
+  //
+  //  static void update() {
+  //    System.out.println("[게시글 변경]");
+  //
+  //    System.out.println("번호? ");
+  //    int index = Integer.parseInt(keyScan.nextLine());
+  //
+  //    if (index < 0 || index >= size) {
+  //      System.out.println("무효한 게시글 번호입니다.");
+  //      return;
+  //    }
+  //
+  //    Board board = boards[index];
+  //
+  //    System.out.printf("제목(%s)? ", board.title);
+  //    String title = keyScan.nextLine();
+  //
+  //    System.out.printf("내용(%s)? ", board.content);
+  //    String content = keyScan.nextLine();
+  //
+  //    System.out.print("정말 변경하시겠습니까?(y/N) ");
+  //    if (!keyScan.nextLine().equals("y")) {
+  //      System.out.println("게시글 변경을 최소하였습니다.");
+  //      return;
+  //    } 
+  //
+  //    board.title = title;
+  //    board.content = content;
+  //
+  //    System.out.println("게시글을 변경하였습니다.");
+  //  }
+  //
+  //  static void delete() {
+  //    System.out.println("[게시글 삭제]");
+  //
+  //    System.out.print("번호? ");
+  //    int index = Integer.parseInt(keyScan.nextLine());
+  //
+  //    if (index < 0 || index >= size) {
+  //      System.out.println("무효한 게시글 번호입니다.");
+  //      return;
+  //    }
+  //
+  //    System.out.print("정말 삭제하시겠습니까?(y/N) ");
+  //    if (!keyScan.nextLine().equals("y")) {
+  //      System.out.println("게시글 삭제를 최소하였습니다.");
+  //      return;
+  //    } 
+  //
+  //    for (int i = index; i < size - 1; i++) {
+  //      boards[i] = boards[i + 1];
+  //    }
+  //
+  //    size--;
+  //
+  //    System.out.println("게시글을 삭제하였습니다.");
+  //  }
+  //
+  //  static void view() {
+  //    System.out.println("[게시글 조회]");
+  //
+  //    System.out.print("번호? ");
+  //    int index = Integer.parseInt(keyScan.nextLine());
+  //
+  //    if (index < 0 || index >= size) {
+  //      System.out.println("무효한 게시글 번호입니다.");
+  //      return;
+  //    }
+  //
+  //    Board board = boards[index];
+  //
+  //    board.viewCount++;
+  //
+  //    System.out.printf("제목: %s\n", board.title);
+  //    System.out.printf("내용: %s\n", board.content);
+  //    System.out.printf("조회수: %d\n", board.viewCount);
+  //    System.out.printf("등록일: %1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS\n", board.createdDate);
+  //  }
 }
-
-
-
-
 
