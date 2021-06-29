b404bb4f9ef48e0910f397603bf02bfe16a64f4a
toinfinityNbeyond <the_1975@naver.com>
2021-06-29 16:10:06 +0900

    0629

--- Day0628/app/src/main/java/Day0628/SJ.java
+++ Day0628/app/src/main/java/Day0628/SJ.java
@@ -3,7 +3,7 @@ package Day0628;
 public class SJ { // make 공급처
 
   public int getTotal( String name, int a, int b) { // 지역변수 이름과 매개변수 이름과 달라야함.
-    System.out.printf(name + "님의 성적합계처리");
+    System.out.println(name + "님의 성적합계처리");
     //    String name1 = "kim";
     int hap = a + b;   // int hap = kor + eng;
     return hap; 
@@ -14,21 +14,48 @@ public class SJ { // make 공급처
     return avg;
   }// end
 
-  public String getScore (String c) {
-    String score = c;
-    return score;
+  public char getGrade (double value) { // 평균데이터 점수 int 정수형
+    char grade = 'F';
+
+    switch ((int)value/10) {
+      case 10:  
+      case 9:   grade = 'A'; break;
+      case 8: grade = 'B'; break;
+      case 7: grade = 'C'; break;
+      case 6: grade = 'D'; break;
+      default :
+        grade = 'F'; break;
+    }
+    return grade;
   }// end
 
-  public boolean getPass(boolean b) {
-    boolean pass = true;
-    return pass; 
+  public String result (double data) { 
+    String msg = "합격여부안내문";
+    if (data >=70)  {
+      msg = "축합격";
+    } else 
+      msg = "재시험";
+    return msg;
+  }// end
+
+  public String result (double data, int x , int y) { // 함수의 중복 = 오버로딩(overLoading)
+    String msg = "합격여부안내문";
+    if (data >=70 && x >= 60 && y >= 60)  {
+      msg = "축합격";
+    } else 
+      msg = "재시험";
+    return msg;
   }// end
 }// class END
 
 
-//합계구하는 함수 sum getTotal
-//평균구하는 함수 avg
-//학점구하는 함수 total
-//합격여부를 구하는 함수 pass
-
+// 합계구하는 함수 sum getTotal
+// 평균구하는 함수 avg
+// 학점구하는 함수 total
+// 합격여부를 구하는 함수 pass
+// 학점을 구하는 메서드 구현 A 100~90  B 80~89 C70~79 C 60~69 D59~0
+// 평균 점수에 따라서 if 대신 switch문 사용
+// 리턴 값, 타입, 메서드 이름 고민해보기 
+// 평균 70점부터 축합격
+// 0~69 재시험
 
--- Day0628/app/src/main/java/Day0628/SJUser.java
+++ Day0628/app/src/main/java/Day0628/SJUser.java
@@ -4,16 +4,23 @@ public class SJUser { // Use 소비자
   public static void  main(String[] args) { //static은 main 
     SJ sj = new SJ();
 
-    //    String name ="kim";
+    // String name ="kim";
     String id = "kim";
-    int first = 90;
-    int second = 85;
+    int first = 100;
+    int second = 55;
     int total = sj.getTotal(id, first, second);
     double avg = sj.getMean(total);
+    char ret = sj.getGrade(avg);
+    // 합격조건 평균이 70점부터, 각 과목별 60점부터
+    // 함수의 중복 = 오버로딩(overLoading)
+    String info =sj.result(avg, first ,second);
+
 
     System.out.println("합계="+ total);
-    System.out.println("합계="+ avg);
-    //    boolean pass = sj.getPass(pass);
-    //    String score = sj.getScore(score);
-  }
+    System.out.println("평균="+ avg);
+    System.out.println("학점=" + ret);
+    System.out.println("결과=" + info);
+
+
+  }//main end
 }// class END
